52a389b6cd71c8b665876e114fd8a8a66b811b6e
cjsgmail <cjs0097@gmail.com>
2022-06-09 16:28:55 +0900

    빌드 에러 수정

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
       <Contents>
         <Title>나에게 맞는 주인님은?</Title>
         <LogoImage>
-          <img src={PangImage} className="rounded-circle" width={350} height={350}></img>
+          <img alt='고양이 사진' src={PangImage} className="rounded-circle" width={350} height={350}></img>
         </LogoImage>
         <Desc>MBTI를 기반으로하는 나에게 맞는 고양이 찾기</Desc>
         <Button onClick={handleClickButton}>테스트 시작하기</Button>
--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -24,7 +24,7 @@ const Result = () => {
       <Contents>
         <Title>결과보기</Title>
         <LogoImage>
-          <img src={resultData.image} className="rounded-circle" width={350} height={350}></img>
+          <img alt='결과이미지' src={resultData.image} className="rounded-circle" width={350} height={350}></img>
         </LogoImage>
         <Desc>예비 집사님과 찰떡궁합인 고양이는 {resultData.name}입니다.</Desc>
         <Button onClick={() => navigate("/")}>
